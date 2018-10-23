c09b099b3c1cc8d0e97101db1555e551adc90905
彭子修 <pengzixiu@pengzixiudeMacBook-Air.local>
2018-10-23 14:41:06 +0800

    save

--- server/server.js
+++ server/server.js
@@ -1,49 +1,72 @@
-require("@babel/polyfill");
-// const path = require('path');
 const express = require('express');
-const React = require('react');
-const Provider = require('react-redux');
-const ReactDOMServer = require('react-dom/server');
-const store = require('../src/store/index.js');
-const BrowserRouter = ("react-router-dom");
 const app = express();
-const App = require('../src/App.js');
+
+// const React = require('react');
+// const Provider = require('react-redux');
+// const ReactDOMServer = require('react-dom/server');
+// const store = require('../src/store/index.js');
+// const BrowserRouter = ("react-router-dom");
+// const App = require('../src/App.js');
 const port = 9093;
 
 app.use('/static', express.static('static'));
-app.use(handleRender);
+// app.use(handleRender);
 
-function handleRender(req, res) {
-    const html = ReactDOMServer.renderToString(
-      <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-      </Provider>
-    )
-    const preloadedState = store.getState();
-    res.send(renderFullPage(html, preloadedState));
+app.get('/*', function (req, res, next){
+  const html = `
+    <div>
+    this is express app ceshi!
+    </div>
+  `
+  res.send(renderFullPage(html))
+})
+
+function renderFullPage(html) {
+  return `
+    <!doctype html>
+    <html>
+      <head>
+        <title>ChatMe</title>
+        <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;" name="viewport" />
+      </head>
+      <body>
+        <div id="root">${html}</div>
+      </body>
+    </html>
+    `
 }
-function renderFullPage(html, preloadedState) {
-    return `
-      <!doctype html>
-      <html>
-        <head>
-          <title>ChatMe</title>
-          <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;" name="viewport" />
-        </head>
-        <body>
-          <div id="root">${html}</div>
-          <script>
-            // 警告：关于在 HTML 中嵌入 JSON 的安全问题，请查看以下文档
-            // http://redux.js.org/recipes/ServerRendering.html#security-considerations
-            window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
-          </script>
-          <script src="/static/bundle.js"></script>
-        </body>
-      </html>
-      `
-}
+
+// function handleRender(req, res) {
+//     const html = ReactDOMServer.renderToString(
+//       <Provider store={store}>
+//         <BrowserRouter>
+//             <App />
+//         </BrowserRouter>
+//       </Provider>
+//     )
+//     const preloadedState = store.getState();
+//     res.send(renderFullPage(html, preloadedState));
+// }
+// function renderFullPage(html, preloadedState) {
+//     return `
+//       <!doctype html>
+//       <html>
+//         <head>
+//           <title>ChatMe</title>
+//           <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;" name="viewport" />
+//         </head>
+//         <body>
+//           <div id="root">${html}</div>
+//           <script>
+//             // 警告：关于在 HTML 中嵌入 JSON 的安全问题，请查看以下文档
+//             // http://redux.js.org/recipes/ServerRendering.html#security-considerations
+//             window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
+//           </script>
+//           <script src="/static/bundle.js"></script>
+//         </body>
+//       </html>
+//       `
+// }
 
 app.listen(port);
 
